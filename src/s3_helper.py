3ab6d3b437bb9158e235b6e7368cbfb43e758dd7
Robin Gagnon <me@reobin.dev>
2020-05-15 10:09:02 -0400

    Move S3 content variables to environment

--- src/s3_helper.py
+++ src/s3_helper.py
@@ -1,21 +1,23 @@
 import boto3
+import os
 
 from print_helper import colors
 
-BUCKET_NAME = "vimcolorschemes"
-REPOSITORIES_DIRECTORY_NAME = "repositories"
+S3_REPOSITORIES_DIRECTORY_NAME = os.getenv("S3_REPOSITORIES_DIRECTORY_NAME")
+S3_BUCKET_NAME = os.getenv("S3_BUCKET_NAME")
+AWS_PROFILE_NAME = os.getenv("AWS_PROFILE_NAME")
 
-session = boto3.session.Session(profile_name="reobin")
+session = boto3.session.Session(profile_name=AWS_PROFILE_NAME)
 s3 = session.resource("s3")
 
 
 def upload_file(file_name, data):
     try:
-        print(f"Writing file {REPOSITORIES_DIRECTORY_NAME}/{file_name} to s3...")
-        s3.Bucket(BUCKET_NAME).put_object(
-            Key=f"{REPOSITORIES_DIRECTORY_NAME}/{file_name}", Body=data
+        print(f"Writing file {S3_REPOSITORIES_DIRECTORY_NAME}/{file_name} to s3...")
+        s3.Bucket(S3_BUCKET_NAME).put_object(
+            Key=f"{S3_REPOSITORIES_DIRECTORY_NAME}/{file_name}", Body=data
         )
     except Exception as e:
         print(
-            f"{colors.ERROR}Error writing file {REPOSITORIES_DIRECTORY_NAME}/{file_name} to s3...\n{e}{colors.NORMAL}"
+            f"{colors.ERROR}Error writing file {S3_REPOSITORIES_DIRECTORY_NAME}/{file_name} to s3...\n{e}{colors.NORMAL}"
         )
