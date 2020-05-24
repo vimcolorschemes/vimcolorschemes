8920d73af568712c52e6cf5e17caf7af5f4a9e31
Robin Gagnon <me@reobin.dev>
2020-05-24 19:36:48 -0400

    chore: Implement cache expiry time .env variable

--- src/request_helper.py
+++ src/request_helper.py
@@ -7,16 +7,21 @@ from print_helper import colors
 TIMEOUT = 5
 
 USE_CACHE = os.getenv("USE_CACHE")
+CACHE_EXPIRE_AFTER = os.getenv("CACHE_EXPIRE_AFTER")
 
 if USE_CACHE:
-    requests_cache.install_cache("github_cache", backend="sqlite", expire_after=3600)
+    requests_cache.install_cache(
+        "github_cache",
+        backend="sqlite",
+        expire_after=CACHE_EXPIRE_AFTER if CACHE_EXPIRE_AFTER is not None else 3600,
+    )
 
 
 def get(url, params={}, auth=None):
+    used_cache = False
     try:
         response = requests.get(url, params=params, timeout=TIMEOUT, auth=auth)
         response.raise_for_status()
-        used_cache = False
         if USE_CACHE:
             used_cache = response.from_cache
         data = response.json()
@@ -24,7 +29,7 @@ def get(url, params={}, auth=None):
     except requests.exceptions.HTTPError as errh:
         if response.status_code == 404:
             print(f"{colors.WARNING}Warning: 404 Not found for {url}{colors.NORMAL}")
-            return None
+            return None, used_cache
         print(f"{colors.ERROR}Http Error:{colors.NORMAL}", errh)
     except requests.exceptions.ConnectionError as errc:
         print(f"{colors.ERROR}Error Connecting:{colors.NORMAL}", errc)
@@ -36,4 +41,4 @@ def get(url, params={}, auth=None):
         print(f"{colors.ERROR}Error decoding JSON response:{colors.NORMAL}", errv)
     except Exception as e:
         print(f"{colors.ERROR}Unexpected error{colors.NORMAL}:", e)
-    return None
+    return None, used_cache
