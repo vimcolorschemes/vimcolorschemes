364d1acad125ec9d93c7182927a9c6ee1c670767
Robin Gagnon <me@reobin.dev>
2020-08-13 19:55:51 -0400

    feat: Add option to customize icon of ExternalLink

--- src/components/externalLink/index.jsx
+++ src/components/externalLink/index.jsx
@@ -2,31 +2,35 @@ import React from "react";
 import PropTypes from "prop-types";
 import classnames from "classnames";
 
-import { ExternalLink as ExternalLinkLinkIcon } from "../icons";
+import { ExternalLink as ExternalLinkIcon } from "../icons";
 
 import "./index.scss";
 
-const ExternalLink = ({ to, className, children, noIcon, ...args }) => (
-  <a
-    href={to}
-    className={classnames(
-      "external-link",
-      { "external-link--inline": noIcon },
-      className,
-    )}
-    target="_blank"
-    rel="noopener noreferrer"
-    {...args}
-  >
-    {children}
-    {!noIcon && <ExternalLinkLinkIcon className="external-link__icon" />}
-  </a>
-);
+const ExternalLink = ({ to, className, children, noIcon, icon, ...args }) => {
+  const Icon = icon || ExternalLinkIcon;
+  return (
+    <a
+      href={to}
+      className={classnames(
+        "external-link",
+        { "external-link--inline": noIcon },
+        className,
+      )}
+      target="_blank"
+      rel="noopener noreferrer"
+      {...args}
+    >
+      {children}
+      {!noIcon && <Icon className="external-link__icon" />}
+    </a>
+  );
+};
 
 ExternalLink.propTypes = {
   to: PropTypes.string.isRequired,
   className: PropTypes.string,
   noIcon: PropTypes.bool,
+  icon: PropTypes.object,
   args: PropTypes.object,
 };
 
