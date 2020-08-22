7731d5f636a0c9fade5d8731ddc08efb308dc884
Robin Gagnon <me@reobin.dev>
2020-08-22 16:53:53 -0400

    fix: Refactor fallback image fetching

--- src/utils/repository.js
+++ src/utils/repository.js
@@ -10,3 +10,13 @@ export const getRepositoryInfos = repository => {
     lastCommitAt: moment(repository.lastCommitAt).fromNow(),
   };
 };
+
+// Returns the usable first image; that was properly processed at build time
+export const getFirstProcessedFluidImage = (featuredImage, images) => {
+  if (featuredImage?.childImageSharp?.fluid)
+    return featuredImage.childImageSharp.fluid;
+
+  if (!images || images.length === 0) return null;
+
+  return images.find(image => !!image?.childImageSharp?.fluid) || null;
+};
