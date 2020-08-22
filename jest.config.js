37cc8cababa0bf8f0ff9269d06d23fe3a6f4d9bd
Robin Gagnon <me@reobin.dev>
2020-08-22 18:03:16 -0400

    Set up tests

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  transform: {
+    "^.+\\.jsx?$": `<rootDir>/jest-preprocess.js`,
+  },
+  testPathIgnorePatterns: [`node_modules`, `\\.cache`, `<rootDir>.*/public`],
+  transformIgnorePatterns: [`node_modules/(?!(gatsby)/)`],
+};
