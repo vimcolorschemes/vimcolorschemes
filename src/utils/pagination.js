e3bee92cf6d4feb21c2b8ddbe66aae6260a13bcc
Robin Gagnon <me@reobin.dev>
2020-09-22 10:17:20 -0400

    Format pagination utils files

--- src/utils/pagination.js
+++ src/utils/pagination.js
@@ -3,6 +3,6 @@ export const paginateRoute = (route, page) => {
   if (!page || typeof page !== "number" || page < 1) page = 1;
 
   const pagePath = page > 1 ? `page/${page}` : "";
-  const routePath = pagePath && !route.endsWith("/") ? `${route}/`: route;
+  const routePath = pagePath && !route.endsWith("/") ? `${route}/` : route;
   return `${routePath}${pagePath}`;
 };
