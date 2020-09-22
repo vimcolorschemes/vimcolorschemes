13dd2a5c6a182c4e2e8fca8e77a436b5bc8c71b8
Tsubasa Ogawa <hurikake@gmail.com>
2020-09-22 21:01:38 +0900

    build: Set up containerized environment (#116)

--- seed/run.sh
+++ seed/run.sh
@@ -1,1 +1,3 @@
-mongoimport --db colorschemes --collection repositories --jsonArray --drop --file $(pwd)/seed/data.json
+SCRIPT_DIR=${REMOTE_SEED_DIR:-$(cd $(dirname $0); pwd)}
+
+mongoimport --db colorschemes --collection repositories --jsonArray --drop --file ${SCRIPT_DIR}/data.json
