ee68f62defdf9f18937ce9d5236b509c9fee2fe7
Vitor Freitas <vitor_freitas_b@hotmail.com>
2020-10-01 11:28:18 -0300

    feat(header): revamp header on mobile

--- src/components/actions/index.jsx
+++ src/components/actions/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import PropTypes from "prop-types";
 import { Link } from "gatsby";
 
@@ -6,27 +6,36 @@ import { LAYOUTS, SECTIONS } from "src/constants";
 
 import "./index.scss";
 
-const Actions = ({ actions, activeAction }) => (
-  <ul className="actions">
-    {actions.map((action, index) => (
-      <li
-        key={`${action.route}-${index}`}
-        className={`actions__item ${
-          activeAction === action ? "actions__item--active" : ""
-        }`}
-      >
-        <Link
-          data-section={SECTIONS.ACTIONS}
-          data-layout={LAYOUTS.LIST}
-          to={action.route}
-          aria-label={`${action.label} vim color schemes`}
+const Actions = ({ actions, activeAction }) => {
+
+  useEffect(() => {
+    document.querySelector('.actions__item--active')
+      .scrollIntoView(false);
+  }, []);
+
+  return (
+    <ul className="actions">
+      {actions.map((action, index) => (
+        <li
+          key={`${action.route}-${index}`}
+          className={`actions__item ${
+            activeAction === action ? "actions__item--active" : ""
+          }`}
         >
-          {action.label}
-        </Link>
-      </li>
-    ))}
-  </ul>
-);
+          <Link
+            data-section={SECTIONS.ACTIONS}
+            data-layout={LAYOUTS.LIST}
+            to={action.route}
+            aria-label={`${action.label} vim color schemes`}
+          >
+            {action.label}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
 
 const actionType = PropTypes.shape({
   label: PropTypes.string.isRequired,
