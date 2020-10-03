11a39f64952872e256d5874fc092cdc23d7c8f52
Robin Gagnon <me@reobin.dev>
2020-10-03 15:55:10 -0400

    refactor: Move emoji converter to its own file (#192)

--- src/utils/string.js
+++ src/utils/string.js
@@ -1,9 +1,2 @@
-import EmojiConvertor from 'emoji-js';
-
-const emoji = new EmojiConvertor();
-emoji.replace_mode = 'unified';
-
 export const URLify = value =>
   !!value ? value.trim().replace(/\s/g, "%20").toLowerCase() : "";
-
-export const parseStringWithEmoji = string => emoji.replace_colons(string);
