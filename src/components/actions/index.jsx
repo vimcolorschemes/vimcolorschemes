ce5c8392e8eed387ae7d96591a2de9350e037ae1
Robin Gagnon <me@reobin.dev>
2020-10-02 20:53:58 -0400

    feat: Improve on actions style on mobile

--- src/components/actions/index.jsx
+++ src/components/actions/index.jsx
@@ -4,38 +4,45 @@ import { Link } from "gatsby";
 
 import { LAYOUTS, SECTIONS } from "src/constants";
 
+import { isInViewport } from "src/utils/navigation";
+
 import "./index.scss";
 
 const Actions = ({ actions, activeAction }) => {
-
   useEffect(() => {
-    document.querySelector('.actions__item--active')
-      .scrollIntoView(false);
+    const activeActionElement = document.querySelector(
+      ".actions__item--active",
+    );
+    if (!isInViewport(activeActionElement))
+      activeActionElement.scrollIntoView({ block: "end", inline: "start" });
   }, []);
 
   return (
-    <ul className="actions">
-      {actions.map((action, index) => (
-        <li
-          key={`${action.route}-${index}`}
-          className={`actions__item ${
-            activeAction === action ? "actions__item--active" : ""
-          }`}
-        >
-          <Link
-            data-section={SECTIONS.ACTIONS}
-            data-layout={LAYOUTS.LIST}
-            to={action.route}
-            aria-label={`${action.label} vim color schemes`}
+    <nav className="actions">
+      <div className="actions__shadow-overlay" />
+      <ul className="actions__list">
+        {actions.map((action, index) => (
+          <li
+            key={`${action.route}-${index}`}
+            className={`actions__item ${
+              activeAction === action ? "actions__item--active" : ""
+            }`}
           >
-            {action.label}
-          </Link>
-        </li>
-      ))}
-    </ul>
+            <Link
+              data-section={SECTIONS.ACTIONS}
+              data-layout={LAYOUTS.LIST}
+              to={action.route}
+              aria-label={`${action.label} vim color schemes`}
+            >
+              {action.label}
+            </Link>
+          </li>
+        ))}
+        <li className="actions__shadow-overlay-block" />
+      </ul>
+    </nav>
   );
-}
-
+};
 
 const actionType = PropTypes.shape({
   label: PropTypes.string.isRequired,
