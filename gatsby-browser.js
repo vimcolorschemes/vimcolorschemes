b7f5126b38e78a3c45a2531fb792db2a8d6be300
Robin Gagnon <me@reobin.dev>
2020-10-17 10:39:38 -0400

    Add comment on previousPath

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,6 +4,7 @@ import "./src/style/reset.css";
 import "./src/style/index.scss";
 
 export const onPreRouteUpdate = () => {
+  // Adds a previousPath property that is available on all pages
   window.locations = window.locations || [document.referrer];
   window.locations.push(window.location.href);
   window.previousPath = window.locations[window.locations.length - 2];
