eee0a5e35907790206eea46692c88967558e8276
Robin Gagnon <me@reobin.dev>
2020-10-17 10:53:33 -0400

    Remove unused code

--- src/elasticsearch/client.js
+++ src/elasticsearch/client.js
@@ -59,22 +59,7 @@ export const bulkIndexRepositories = async (client, repositories) => {
       repository,
     ]);
 
-    const { body: bulkResponse } = await client.bulk({ refresh: true, body });
-
-    if (bulkResponse.errors) {
-      const erroredDocuments = [];
-      bulkResponse.items.forEach((action, i) => {
-        const operation = Object.keys(action)[0];
-        if (action[operation].error) {
-          erroredDocuments.push({
-            status: action[operation].status,
-            error: action[operation].error,
-            operation: body[i * 2],
-            document: body[i * 2 + 1],
-          });
-        }
-      });
-    }
+    await client.bulk({ refresh: true, body });
 
     const {
       body: { count },
