e0a4861b772de3eb5bec5452269d8a709e185333
Robin Gagnon <me@reobin.dev>
2021-10-24 11:15:13 -0500

    feat: Improve responsiveness on all pages (#397)

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -5,6 +5,7 @@ import Routes from '@/lib/routes';
 
 import ExternalLink from '@/components/externalLink';
 import HomeLink from '@/components/homeLink';
+import IconGithub from '@/components/icons/github';
 
 import './index.scss';
 
@@ -16,20 +17,40 @@ interface Props {
 function Footer({ isHome, onHomeLinkClick }: Props) {
   return (
     <footer className="footer">
-      <div className="footer__links">
-        <HomeLink isHome={isHome} onClick={onHomeLinkClick} isFooter />
-      </div>
-      <div className="footer__links">
-        <Link to={Routes.About} data-focusable data-testid="footer__about">
-          About
-        </Link>
-        <ExternalLink to={Routes.Github} data-focusable>
-          Github
-        </ExternalLink>
-        <ExternalLink to={Routes.Contact} data-focusable>
-          Contact
-        </ExternalLink>
-      </div>
+      <nav className="footer__content" arial-label="footer navigation links">
+        <div className="footer__links">
+          <HomeLink isHome={isHome} onClick={onHomeLinkClick} isFooter />
+        </div>
+        <div aria-label="footer links" className="footer__links">
+          <Link
+            to={Routes.About}
+            data-focusable
+            data-testid="footer__about"
+            className="footer__link"
+          >
+            About
+          </Link>
+          <ExternalLink
+            to={Routes.Github}
+            data-focusable
+            className="footer__link footer__link--accent"
+          >
+            <span>
+              Follow{' '}
+              <span className="footer__link-extension">vimcolorshemes</span> on
+              Github
+            </span>
+            <IconGithub />
+          </ExternalLink>
+          <ExternalLink
+            to={Routes.Contact}
+            data-focusable
+            className="footer__link"
+          >
+            Contact
+          </ExternalLink>
+        </div>
+      </nav>
     </footer>
   );
 }
