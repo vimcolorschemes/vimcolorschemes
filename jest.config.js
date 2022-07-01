878872960519a9aa028c2de346c1e9ac09dc70fc
Robin Gagnon <me@reobin.dev>
2022-07-01 16:24:26 -0500

    fix(deps): Resolve jest and puppeteer dependencies (#700)

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 const { compilerOptions } = require('./tsconfig.json');
-const { pathsToModuleNameMapper } = require('ts-jest/utils');
+const { pathsToModuleNameMapper } = require('ts-jest');
 const paths = pathsToModuleNameMapper(compilerOptions.paths, {
   prefix: '<rootDir>/',
 });
@@ -9,21 +9,24 @@ module.exports = {
     '^.+\\.[jt]sx?$': '<rootDir>/jest-preprocess.js',
   },
   moduleNameMapper: {
-    '.+\\.(css|styl|less|sass|scss)$': `identity-obj-proxy`,
-    '.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': `<rootDir>/__mocks__/file-mock.js`,
+    '.+\\.(css|styl|less|sass|scss)$': 'identity-obj-proxy',
+    '.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/__mocks__/file-mock.js',
     ...paths,
   },
   testPathIgnorePatterns: [
-    `node_modules`,
-    `cypress`,
-    `\\.cache`,
-    `<rootDir>.*/public`,
+    'node_modules',
+    'cypress',
+    '\\.cache',
+    '<rootDir>.*/public',
   ],
-  transformIgnorePatterns: [`node_modules/(?!(gatsby)/)`],
+  transformIgnorePatterns: ['node_modules/(?!(gatsby)/)'],
   globals: {
-    __PATH_PREFIX__: ``,
+    __PATH_PREFIX__: '',
   },
-  testURL: `http://localhost`,
-  setupFiles: [`<rootDir>/loadershim.js`],
+  testEnvironmentOptions: {
+    url: 'http://localhost',
+  },
+  setupFiles: ['<rootDir>/loadershim.js'],
   testEnvironment: 'jsdom',
 };
