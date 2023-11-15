473da884672faefb72fe98f91405b8bdb862f688
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-11-15 15:12:18 -0800

    chore: codemod src/**/*.ts

--- src/helpers/s3.ts
+++ src/helpers/s3.ts
@@ -1,8 +1,11 @@
-import AWS from 'aws-sdk';
+import { Upload } from '@aws-sdk/lib-storage';
+import { S3 } from '@aws-sdk/client-s3';
 
-const s3 = new AWS.S3({
-  accessKeyId: process.env.AWS_S3_ACCESS_KEY_ID || '',
-  secretAccessKey: process.env.AWS_S3_SECRET_ACCESS_KEY || '',
+const s3 = new S3({
+  credentials: {
+    accessKeyId: process.env.AWS_S3_ACCESS_KEY_ID || '',
+    secretAccessKey: process.env.AWS_S3_SECRET_ACCESS_KEY || '',
+  },
 });
 
 /*
@@ -19,11 +22,14 @@ async function upload(
 ): Promise<void> {
   await remove(bucket, key);
   const body = JSON.stringify(payload);
-  await s3.upload({ Bucket: bucket, Key: key, Body: body }).promise();
+  await new Upload({
+    client: s3,
+    params: { Bucket: bucket, Key: key, Body: body },
+  }).done();
 }
 
 async function remove(bucket: string, key: string) {
-  await s3.deleteObject({ Bucket: bucket, Key: key }, () => {}).promise();
+  await s3.deleteObject({ Bucket: bucket, Key: key }, () => {});
 }
 
 const S3 = { upload };
