817d35d78777eb22abe5b1bbe75a2e489701751c
Robin Gagnon <me@reobin.dev>
2024-03-17 12:19:30 -0500

    next: Add header and footer to root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import '@/styles/reset.css';
 import '@/styles/globals.css';
+import { ReactNode } from 'react';
+import Header from '@/components/header';
+import Footer from '@/components/footer';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -14,14 +17,18 @@ export const metadata: Metadata = {
     'Check out the Trending vim color schemes! | vimcolorschemes is the ultimate resource for vim users to find the perfect color scheme for their favorite development environment. Come for the hundreds of vim color schemes, stay for the awesome hjkl spatial navigation.',
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+interface RootLayoutProps {
+  children: ReactNode;
+}
+
+export default function RootLayout({ children }: Readonly<RootLayoutProps>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Header />
+        {children}
+        <Footer />
+      </body>
     </html>
   );
 }
---
+++ src/components/footer/index.tsx
@@ -0,0 +1,3 @@
+export default function Footer() {
+  return <footer>footer</footer>;
+}
