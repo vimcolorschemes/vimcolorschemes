a55ed6352747b74d38f08f70d09e38c134b05208
Robin Gagnon <me@reobin.dev>
2024-03-17 11:55:49 -0500

    next: Move styles to their own directory

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
-import './reset.css';
-import './globals.css';
+import '@/styles/reset.css';
+import '@/styles/globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
 
