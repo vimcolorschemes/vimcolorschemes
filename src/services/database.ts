fa63ff8d376dfa045e116da69594432763a0457c
Robin Gagnon <me@reobin.dev>
2024-06-21 11:20:49 -0500

    refactor: Cache MongoDB connections

--- src/services/database.ts
+++ src/services/database.ts
@@ -1,17 +1,44 @@
-import mongoose from 'mongoose';
+import mongoose, { Mongoose } from 'mongoose';
+
+declare global {
+  var mongoose: { conn: Mongoose | null; promise: Promise<Mongoose> | null };
+}
+
+const CONNECTION_STRING: string = process.env.DATABASE_CONNECTION_STRING!;
+if (!CONNECTION_STRING) {
+  throw new Error(
+    'Please define the DATABASE_CONNECTION_STRING environment variable.',
+  );
+}
+
+let cached = global.mongoose;
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
 
 /**
- * Connect to the database. Throws an error if the necessary environment
- * variables are not set.
- *
+ * Connect to the database.
  * @returns A promise that resolves to the mongoose instance.
  */
-function connect(): Promise<typeof mongoose> {
-  if (typeof process.env.DATABASE_CONNECTION_STRING !== 'string') {
-    throw new Error('DATABASE_CONNECTION_STRING is not set');
+async function connect(): Promise<typeof mongoose> {
+  if (cached.conn) {
+    return cached.conn;
   }
 
-  return mongoose.connect(process.env.DATABASE_CONNECTION_STRING);
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(CONNECTION_STRING, {
+      bufferCommands: false,
+    });
+  }
+
+  try {
+    cached.conn = await cached.promise;
+  } catch (e) {
+    cached.promise = null;
+    throw e;
+  }
+
+  return cached.conn;
 }
 
 const DatabaseService = { connect };
