705e6818b234f64208801ddb11f993059ce279de
Robin Gagnon <me@reobin.dev>
2025-01-28 08:28:25 -0500

    chore: Use `let` over `var` for database connection

--- src/services/database.ts
+++ src/services/database.ts
@@ -1,6 +1,7 @@
 import mongoose, { Mongoose } from 'mongoose';
 
 declare global {
+  // eslint-disable-next-line no-var
   var mongoose: { conn: Mongoose | null; promise: Promise<Mongoose> | null };
 }
 
