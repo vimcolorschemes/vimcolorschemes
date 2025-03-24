31984e0d68017d0296b18b7c16c68c20affa30b6
Robin Gagnon <me@reobin.dev>
2025-03-24 10:23:30 -0500

    remove unused avatar URL prop

--- src/models/owner.ts
+++ src/models/owner.ts
@@ -6,10 +6,6 @@ type Owner = {
    * The owner Github username.
    */
   name: string;
-  /**
-   * The owner Github account avatar URL.
-   */
-  avatarURL: string | null;
 };
 
 export default Owner;
