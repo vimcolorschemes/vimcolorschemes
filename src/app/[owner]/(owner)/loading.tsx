e5387e7cf2f57c6d8ab95fbe809555924e30532f
Robin Gagnon <me@reobin.dev>
2025-03-29 14:03:36 -0500

    feat: Add owner avatar to artist page and repo title (#1163)

--- src/app/[owner]/(owner)/loading.tsx
+++ src/app/[owner]/(owner)/loading.tsx
@@ -2,11 +2,13 @@ import OwnerPageHeaderSkeleton from '@/components/ownerPageHeader/skeleton';
 import OwnerTitle from '@/components/ownerTitle';
 import RepositoriesSkeleton from '@/components/repositories/skeleton';
 
+import styles from './page.module.css';
+
 export default function OwnerPageLoading() {
   return (
     <>
       <OwnerPageHeaderSkeleton />
-      <OwnerTitle />
+      <OwnerTitle className={styles.owner} />
       <RepositoriesSkeleton />
     </>
   );
