6613a2eb33a87bef8b7e2fedb65430bcfe0339f4
Robin Gagnon <me@reobin.dev>
2025-05-03 17:15:26 -0500

    feat: Use cache on db requests

--- src/components/repositories/count/index.tsx
+++ src/components/repositories/count/index.tsx
@@ -1,11 +1,19 @@
+import { unstable_cache } from 'next/cache';
 import { redirect } from 'next/navigation';
 
 import RepositoriesService from '@/services/repositories';
 
 import Constants from '@/lib/constants';
+import Filter from '@/lib/filter';
 import PageContext from '@/lib/pageContext';
 
 import FilterHelper from '@/helpers/filter';
+
+const getRepositoryCount = unstable_cache(
+  (filter: Filter) => RepositoriesService.getRepositoryCount(filter),
+  ['getRepositoryCount'],
+  { revalidate: 60 },
+);
 
 type RepositoriesCountProps = {
   pageContext: PageContext;
@@ -14,9 +22,7 @@ type RepositoriesCountProps = {
 export default async function RepositoriesCount({
   pageContext,
 }: RepositoriesCountProps) {
-  const count = await RepositoriesService.getRepositoryCount(
-    pageContext.filter,
-  );
+  const count = await getRepositoryCount(pageContext.filter);
 
   const pageCount = Math.ceil(count / Constants.REPOSITORY_PAGE_SIZE);
   if ((pageContext.filter.page || 1) > (pageCount || 1)) {
