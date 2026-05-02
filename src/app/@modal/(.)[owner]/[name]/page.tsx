b0d7a0b6119c410f59fe4a69d0650d694435d5f7
Robin Gagnon <me@reobin.dev>
2026-05-01 19:01:18 -0500

    feat: show repository pages in a modal (#1331)

---
+++ src/app/@modal/(.)[owner]/[name]/page.tsx
@@ -0,0 +1,30 @@
+import { RepositoriesService } from '@/services/repositoriesServer';
+
+import RepositoryPageContent from '@/components/repositoryPageContent';
+import RepositoryPageModal from '@/components/repositoryPageModal';
+
+export const dynamicParams = false;
+
+export async function generateStaticParams() {
+  const keys = await RepositoriesService.getAllRepositoryKeys();
+  return keys.map(k => ({
+    owner: k.ownerName.toLowerCase(),
+    name: k.name.toLowerCase(),
+  }));
+}
+
+type RepositoryPageModalRouteProps = {
+  params: Promise<{ owner: string; name: string }>;
+};
+
+export default async function RepositoryPageModalRoute({
+  params,
+}: RepositoryPageModalRouteProps) {
+  const { owner, name } = await params;
+
+  return (
+    <RepositoryPageModal>
+      <RepositoryPageContent owner={owner} name={name} />
+    </RepositoryPageModal>
+  );
+}
--- src/app/[owner]/[name]/loading.tsx
+++ src/app/[owner]/[name]/loading.tsx
@@ -1,9 +1,8 @@
 import ColorschemesGridSkeleton from '@/components/colorschemesGrid/skeleton';
 import RepositoryInfo from '@/components/repositoryInfo/repositoryInfo';
+import styles from '@/components/repositoryPageContent/index.module.css';
 import RepositoryPageHeaderSkeleton from '@/components/repositoryPageHeader/skeleton';
 import RepositoryTitle from '@/components/repositoryTitle';
-
-import styles from './page.module.css';
 
 export default function RepositoryPageLoading() {
   return (
